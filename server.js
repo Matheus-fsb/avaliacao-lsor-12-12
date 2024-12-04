2e8002c46659470c43fd09a4dbd5804a62272664
Matheus-fsb <mfsb010507@gmail.com>
2024-12-04 09:55:03 -0300

    atualização

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const app = require('./src/app');
 const sequelize = require('./src/database/database');
 require('dotenv').config();
 
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
-// Sincroniza o banco de dados com a aplicação
-sequelize.sync({ force: false })
-  .then(() => {
-    // Inicia o servidor quando o banco de dados for sincronizado com sucesso
-    app.listen(port, () => {
-      console.log(`Servidor iniciado em: ${port}`);
-      console.log('Banco de dados sincronizado com sucesso.');
+const Users = require('./src/models/Users')
+
+(async () => {
+  try {
+    await Users.sync({ force: true });
+
+    console.log('Database sincronizado com sucesso.');
+
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
-  })
-  .catch((error) => {
-    // Exibe um alerta de erro caso a sincronização do banco de dados falhe
-    console.error('Erro ao sincronizar o banco de dados:', error.message);
-  });
+
+  } catch (error) {
+    console.log('Erro ao sincronizar o banco de dados:', error);
+  }
+})();
+
